f7ebcd23ad2a8ef26df4286744a78675fd70af74
Vasiliy Tyubek <v.tyubek@gmail.com>
2024-06-28 01:05:58 +0300

    Update and rename world.py to hello.py

--- dags/hello.py
+++ dags/hello.py
@@ -7,7 +7,7 @@ from airflow.operators.empty import EmptyOperator
 
 with DAG(
 	dag_id="my_dag_name",
-	start_date=datetime.datetime(2021, 1, 1),
+	start_date=datetime.datetime(2024, 6, 20),
 	schedule="@daily",
 ):
 	EmptyOperator(task_id="task")
