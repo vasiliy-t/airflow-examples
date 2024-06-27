6070786b393bc29eb1c82eb0a095cbcc1ce4d332
Vasiliy Tyubek <v.tyubek@gmail.com>
2024-06-28 02:00:28 +0300

    Update and rename hello_2.py to hello_3.py

--- dags/hello_3.py
+++ dags/hello_3.py
@@ -6,7 +6,7 @@ from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 
 with DAG(
-	dag_id="my_dag_name_2",
+	dag_id="my_dag_name_3",
 	start_date=datetime.datetime(2024, 6, 20),
 	schedule="@daily",
 ):
