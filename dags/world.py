265ddd84b1022284c2b29d333ac4cd557f7a310e
Vasiliy Tyubek <v.tyubek@gmail.com>
2024-06-28 00:56:58 +0300

    Create world.py

---
+++ dags/world.py
@@ -0,0 +1,13 @@
+"""Example DAG demonstrating the usage of the BashOperator."""
+
+import datetime
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+	dag_id="my_dag_name",
+	start_date=datetime.datetime(2021, 1, 1),
+	schedule="@daily",
+):
+	EmptyOperator(task_id="task")
